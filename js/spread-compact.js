e435787c94df294bbcdbe2adfb1906cb47eadfc0
idrfe <142594514+idrfe@users.noreply.github.com>
2023-08-18 14:05:17 +0200

    Add files via upload

---
+++ js/spread-compact.js
@@ -0,0 +1,90 @@
+const EOP = [{"x": 0.875, "y": 8.5356, "sourcename": "Banco de Bogot\u00e1"}, {"x": 0.875, "y": 9.0, "sourcename": "BBVA Research"}, {"x": 0.875, "y": 7.4, "sourcename": "Citigroup Global Mkts"}, {"x": 0.875, "y": 9.8, "sourcename": "Corficolombiana"}, {"x": 0.875, "y": 8.35, "sourcename": "Credicorp Capital"}, {"x": 0.875, "y": 7.699999809265137, "sourcename": "Credit Suisse"}, {"x": 0.875, "y": 9.42305, "sourcename": "Ecoanal\u00edtica"}, {"x": 0.875, "y": 7.7, "sourcename": "EIU"}, {"x": 0.875, "y": 9.19114143068032, "sourcename": "HSBC"}, {"x": 0.875, "y": 8.377, "sourcename": "IMF"}, {"x": 0.875, "y": 9.3, "sourcename": "Ita\u00fa Unibanco"}, {"x": 0.875, "y": 6.74734, "sourcename": "Torino Capital"}, {"x": 0.875, "y": 7.22860186188268, "sourcename": "BancTrust & Co."}, {"x": 0.875, "y": 8.0, "sourcename": "AGPV"}, {"x": 0.875, "y": 8.99815, "sourcename": "Bancolombia"}, {"x": 0.875, "y": 8.10115012900912, "sourcename": "Barclays Capital"}, {"x": 0.875, "y": 6.0, "sourcename": "CABI"}, {"x": 0.875, "y": 10.0063, "sourcename": "Capital Economics"}, {"x": 0.875, "y": 8.5, "sourcename": "Fedesarrollo"}, {"x": 0.875, "y": 7.7, "sourcename": "JPMorgan"}, {"x": 0.875, "y": 8.9, "sourcename": "Positiva Compa\u00f1\u00eda de Seguros"}, {"x": 0.875, "y": 9.83194884748553, "sourcename": "Banco Agrario de Colombia"}, {"x": 0.875, "y": 8.9, "sourcename": "Asobancaria"}, {"x": 1.875, "y": 4.0, "sourcename": "AGPV"}, {"x": 1.875, "y": 4.8, "sourcename": "Bancolombia"}, {"x": 1.875, "y": 4.00434106242187, "sourcename": "Barclays Capital"}, {"x": 1.875, "y": 4.36394, "sourcename": "Capital Economics"}, {"x": 1.875, "y": 5.0, "sourcename": "Fedesarrollo"}, {"x": 1.875, "y": 4.7, "sourcename": "JPMorgan"}, {"x": 1.875, "y": 4.5, "sourcename": "Positiva Compa\u00f1\u00eda de Seguros"}, {"x": 1.875, "y": 6.36477265297435, "sourcename": "Banco Agrario de Colombia"}, {"x": 1.875, "y": 5.2, "sourcename": "Asobancaria"}, {"x": 1.875, "y": 3.7, "sourcename": "Citigroup Global Mkts"}, {"x": 1.875, "y": 5.0, "sourcename": "Corficolombiana"}, {"x": 1.875, "y": 4.0, "sourcename": "Credicorp Capital"}, {"x": 1.875, "y": 4.599999904632568, "sourcename": "Credit Suisse"}, {"x": 1.875, "y": 4.1284, "sourcename": "Ecoanal\u00edtica"}, {"x": 1.875, "y": 4.2, "sourcename": "EIU"}, {"x": 1.875, "y": 5.0, "sourcename": "HSBC"}, {"x": 1.875, "y": 3.492, "sourcename": "IMF"}, {"x": 1.875, "y": 4.2, "sourcename": "Ita\u00fa Unibanco"}, {"x": 1.875, "y": 4.69783510089092, "sourcename": "BancTrust & Co."}, {"x": 0.875, "y": 8.61, "sourcename": "Fitch Solutions"}, {"x": 0.875, "y": 9.180028205816, "sourcename": "Goldman Sachs"}, {"x": 0.875, "y": 9.7, "sourcename": "Oxford Economics"}, {"x": 0.875, "y": 9.62776158370597, "sourcename": "Moody's Analytics"}, {"x": 0.875, "y": 11.003, "sourcename": "S&P Global Ratings"}, {"x": 0.875, "y": 9.23, "sourcename": "Scotiabank Colpatria"}, {"x": 0.875, "y": 7.8, "sourcename": "EmergingMarketWatch"}, {"x": 0.875, "y": 8.7, "sourcename": "Alianza Valores y Fiduciaria"}, {"x": 1.875, "y": 4.23823, "sourcename": "Banco de Bogot\u00e1"}, {"x": 1.875, "y": 5.0,"sourcename": "BBVA Research"}, {"x": 1.875, "y": 4.05, "sourcename": "Fitch Solutions"}, {"x": 1.875, "y": 4.3, "sourcename": "Goldman Sachs"}, {"x": 1.875, "y": 4.16013, "sourcename": "Oxford Economics"}, {"x": 1.875, "y": 4.70156285380134, "sourcename": "Moody's Analytics"}, {"x": 1.875, "y": 4.281, "sourcename": "S&P Global Ratings"}, {"x": 1.875, "y": 4.92, "sourcename": "Scotiabank Colpatria"}, {"x": 1.875, "y": 4.0, "sourcename": "EmergingMarketWatch"}, {"x": 1.875, "y": 3.81, "sourcename": "Alianza Valores y Fiduciaria"}, {"x": 0.875, "y": 10.0404492120743, "sourcename": "Banco Davivienda"}, {"x": 0.875, "y": 7.39128350742223, "sourcename": "Pezco Economics"}, {"x": 0.875, "y": 8.39213168705058, "sourcename": "UBS"}, {"x": 0.875, "y": 8.5, "sourcename": "Econosignal"}, {"x": 0.875, "y": 8.6, "sourcename": "CAF"}, {"x": 0.875,"y": 6.965, "sourcename": "Acciones y Valores"}, {"x": 1.875, "y": 5.38391268324341, "sourcename": "Banco Davivienda"}, {"x": 1.875, "y": 6.73692526113335, "sourcename": "Pezco Economics"}, {"x": 1.875, "y": 4.251005281522, "sourcename": "UBS"}, {"x": 1.875, "y": 3.96, "sourcename": "Econosignal"}, {"x": 1.875, "y": 4.1, "sourcename": "CAF"}, {"x": 1.875, "y": 3.84, "sourcename": "Acciones y Valores"}];
+const AOP = [{"x": 1.125, "y": 7.3, "sourcename": "Allianz"}, {"x": 1.125, "y": 10.8587, "sourcename": "Citigroup Global Mkts"}, {"x": 1.125, "y": 11.9, "sourcename": "Corficolombiana"}, {"x": 1.125, "y": 10.9, "sourcename": "Credicorp Capital"}, {"x": 1.125, "y": 10.9, "sourcename": "Credit Suisse"}, {"x": 1.125, "y": 11.8601, "sourcename": "Ecoanal\u00edtica"}, {"x": 1.125, "y": 11.9, "sourcename": "EIU"}, {"x": 1.125, "y": 11.4794263144401, "sourcename": "HSBC"}, {"x": 1.125, "y": 10.854, "sourcename": "IMF"}, {"x": 1.125, "y": 12.2651, "sourcename": "Julius Baer"},{"x": 1.125, "y": 11.199999809265137, "sourcename": "Standard Chartered"}, {"x": 1.125, "y": 10.4234, "sourcename": "Torino Capital"}, {"x": 1.125, "y": 8.9148533765254, "sourcename": "BancTrust & Co."}, {"x": 2.125, "y": 4.57135, "sourcename": "Citigroup Global Mkts"}, {"x": 2.125, "y": 6.9, "sourcename": "Corficolombiana"}, {"x": 2.125, "y": 6.2, "sourcename": "Credicorp Capital"}, {"x": 2.125, "y": 5.0, "sourcename": "Credit Suisse"}, {"x": 2.125, "y": 5.41967, "sourcename": "Ecoanal\u00edtica"}, {"x": 2.125, "y": 4.0, "sourcename": "EIU"}, {"x": 2.125, "y": 5.4713352025328, "sourcename": "HSBC"}, {"x": 2.125, "y": 5.436, "sourcename": "IMF"}, {"x": 2.125, "y": 5.41579, "sourcename": "Julius Baer"}, {"x": 2.125, "y": 5.599999904632568, "sourcename": "Standard Chartered"}, {"x": 2.125, "y": 5.83912370217472, "sourcename": "BancTrust & Co."}, {"x": 1.125, "y": 9.89971, "sourcename": "Banco de Bogot\u00e1"}, {"x": 1.125, "y": 12.0, "sourcename": "BBVA Research"}, {"x": 1.125, "y": 10.91, "sourcename": "Fitch Solutions"}, {"x": 1.125, "y": 11.551226153832, "sourcename": "Goldman Sachs"}, {"x": 1.125, "y": 12.0474, "sourcename": "Oxford Economics"}, {"x": 1.125, "y": 11.2, "sourcename": "World Bank"}, {"x": 1.125, "y": 9.5, "sourcename": "Euromonitor Int."}, {"x": 1.125, "y": 11.7407284195453, "sourcename": "Moody's Analytics"}, {"x": 1.125, "y": 11.003, "sourcename": "S&P Global Ratings"}, {"x": 1.125, "y": 12.0, "sourcename": "Scotiabank Colpatria"}, {"x": 2.125, "y": 4.92893, "sourcename": "Banco de Bogot\u00e1"}, {"x": 2.125, "y": 6.2, "sourcename": "BBVA Research"}, {"x": 2.125, "y": 5.78, "sourcename": "Fitch Solutions"}, {"x": 2.125, "y": 5.798460341317, "sourcename": "Goldman Sachs"}, {"x": 2.125, "y": 5.43948, "sourcename": "Oxford Economics"}, {"x": 2.125, "y": 6.3, "sourcename": "World Bank"}, {"x": 2.125, "y": 4.99, "sourcename": "Euromonitor Int."}, {"x": 2.125, "y": 5.59621477011429, "sourcename": "Moody's Analytics"}, {"x": 2.125, "y": 4.281, "sourcename": "S&P Global Ratings"}, {"x": 2.125, "y": 5.73, "sourcename": "Scotiabank Colpatria"}, {"x": 1.125, "y": 11.6938, "sourcename": "Bancolombia"}, {"x": 1.125, "y": 11.330731836532, "sourcename": "Barclays Capital"}, {"x": 1.125, "y": 12.0, "sourcename": "Capital Economics"}, {"x": 1.125, "y": 11.2, "sourcename": "JPMorgan"}, {"x": 1.125, "y": 11.74, "sourcename": "Positiva Compa\u00f1\u00eda de Seguros"}, {"x": 1.125, "y": 10.0, "sourcename": "Fitch Ratings"}, {"x": 1.125, "y": 11.0, "sourcename": "Pantheon Macroeconomics"}, {"x":1.125, "y": 12.0473464341177, "sourcename": "Banco Agrario de Colombia"}, {"x": 1.125, "y": 11.53, "sourcename": "Asobancaria"}, {"x": 2.125, "y": 6.71942, "sourcename": "Bancolombia"}, {"x": 2.125, "y": 5.05324879662397, "sourcename": "Barclays Capital"}, {"x": 2.125, "y": 5.5, "sourcename": "Capital Economics"}, {"x": 2.125, "y": 5.4, "sourcename": "JPMorgan"}, {"x": 2.125, "y": 5.63, "sourcename": "Positiva Compa\u00f1\u00eda de Seguros"}, {"x": 2.125, "y": 6.0, "sourcename": "Fitch Ratings"}, {"x": 2.125, "y": 4.5, "sourcename": "Pantheon Macroeconomics"}, {"x": 2.125, "y": 7.36723260496577, "sourcename": "Banco Agrario de Colombia"}, {"x": 2.125, "y": 6.72, "sourcename": "Asobancaria"}, {"x": 1.125, "y": 12.0610073043374, "sourcename": "Banco Davivienda"}, {"x": 1.125, "y": 10.6, "sourcename": "DekaBank"}, {"x": 1.125, "y": 9.6,"sourcename": "FrontierView"}, {"x": 1.125, "y": 10.8, "sourcename": "Kiel Institute"}, {"x": 1.125, "y": 8.81846263056667, "sourcename": "Pezco Economics"}, {"x": 1.125, "y": 10.9123, "sourcename": "Soci\u00e9t\u00e9 G\u00e9n\u00e9rale"}, {"x": 1.125, "y": 11.4309115906768, "sourcename": "UBS"}, {"x": 1.125, "y": 11.49, "sourcename": "Econosignal"}, {"x": 1.125, "y": 9.0, "sourcename": "TD Securities"}, {"x": 1.125, "y": 11.3004596925589, "sourcename": "Acciones y Valores"}, {"x": 2.125, "y": 6.81359443642609, "sourcename": "Banco Davivienda"}, {"x": 2.125, "y": 3.2, "sourcename": "DekaBank"}, {"x": 2.125, "y": 5.4, "sourcename": "FrontierView"}, {"x": 2.125, "y": 5.1, "sourcename": "Kiel Institute"}, {"x": 2.125, "y": 7.06410438427779, "sourcename": "Pezco Economics"}, {"x": 2.125, "y": 5.70183, "sourcename": "Soci\u00e9t\u00e9 G\u00e9n\u00e9rale"}, {"x": 2.125, "y": 5.21334521650387, "sourcename": "UBS"}, {"x": 2.125, "y": 5.49, "sourcename": "Econosignal"}, {"x": 2.125, "y": 4.7, "sourcename": "TD Securities"}, {"x": 2.125, "y": 5.24017031877468, "sourcename": "Acciones y Valores"}]
+const markerSize = 5;
+const jitteramount = 0.125;
+
+function addJitterToData(data, jitterAmount) {
+return data.map(point => ({
+  x: point.x + (Math.random() - 0.5) * jitterAmount,
+  y: point.y,
+  sourcename: point.sourcename
+}));
+}
+
+
+// Create the chart
+Highcharts.chart('spread-container', {
+chart: {
+  backgroundColor: '#eaeaea'
+},
+credits: {
+  enabled: false // Disable the Highcharts footnote
+},
+title: {
+  text: ''
+},
+xAxis: {
+  title: {
+    text: ''
+  },
+  tickPositions: [1, 2], // Set the desired x-axis values (with margins)
+  labels: {
+    formatter: function() {
+      return this.value === 1 ? '2023' : this.value === 2 ? '2024' : ''; // Custom x-axis labels
+    }
+  },
+  min: 0.5, // Start the x-axis at 0 (left margin)
+  max: 2.5,
+  lineColor: 'transparent',
+  // End the x-axis at 3 (right margin)
+},
+yAxis: {
+  tickAmount:5,
+  title: {
+    text: ''
+  },
+
+},
+plotOptions: {
+  scatter: {
+    marker: {
+      symbol: 'circle',
+      radius: markerSize
+    }
+  }
+},
+legend: {
+  layout: 'horizontal', // Set the legend layout to vertical
+  align: 'center', // Align the legend to the right
+  verticalAlign: 'bottom', // Center the legend vertically
+  symbolRadius: 4 // Set the size of the legend symbols to match the scatter plot symbols
+},
+exporting: {
+  enabled: false
+},
+tooltip: {
+
+  formatter: function() {
+    return `Source: ${this.point.sourcename}<br>Value: ${this.point.y}`;
+  },
+  style: {
+    opacity: 1 // Set the desired opacity value (0.8 in this example)
+  },
+
+},
+series: [{
+    type: 'scatter', // Scatter plot for period 1
+    name: 'End of period',
+    axis: 0,
+    color: '#6ac8ed',
+    data: addJitterToData(EOP, jitteramount)
+  },
+  {
+    type: 'scatter', // Scatter plot for period 2
+    name: 'Avg. of period',
+    axis: 0,
+    color: '#013058',
+    data: addJitterToData(AOP, jitteramount)
+  }
+]
+});
